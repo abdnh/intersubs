2c2013d393b9f919667876000f5c08b522e972e0
Abdo <abd.nh25@gmail.com>
2022-07-14 20:48:47 +0300

    Add setup.py and rename src dir

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="intersubs",
+    version="0.0.1",
+    description="Interactive subtitles for mpv",
+    author="Abdo",
+    author_email="abd.nh25@gmail.com",
+    url="https://github.com/abdnh/intersubs",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["intersubs"],
+    install_requires=["pywin32", "PyQt5", "PyQtWebEngine"],
+)
